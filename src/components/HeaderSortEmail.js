98cea9c477391f4c3758d13a15c7f04deee754a4
Marta Radziszewska <marta.radziszewskaa@gmail.com>
2018-09-05 21:27:14 +0200

    remove unused class

--- src/components/HeaderSortEmail.js
+++ src/components/HeaderSortEmail.js
@@ -4,8 +4,7 @@ import React from 'react';
 export default class HeaderSortEmail extends React.Component{
     render(){
         return(
-            <th className="sort-btn"
-                colSpan = "2"
+            <th colSpan = "2"
                 onClick={this.props.sortByEmail}>
                 {this.props.header}
                 <i className = {this.props.direction ? "fas fa-sort-up" : "fas fa-sort-down"}></i>
--- src/components/HeaderSortName.js
+++ src/components/HeaderSortName.js
@@ -4,8 +4,7 @@ import React from 'react';
 export default class HeaderSortName extends React.Component{
     render(){
         return(
-            <th className="sort-btn"
-                onClick={this.props.sortByName}>
+            <th onClick={this.props.sortByName}>
                 {this.props.header}
                 <i className = {this.props.direction ? "fas fa-sort-up" : "fas fa-sort-down"}></i>
             </th>
